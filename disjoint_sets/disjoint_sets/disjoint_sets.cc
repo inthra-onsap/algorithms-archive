ca13f5a2c73a350dd049b587e82ba94ce71eac22
inthra.onsap <inthra.onsap@gmail.com>
2017-05-21 23:07:15 +0700

    Change DisjointSets UnionByHeight to UnionBySize to support PathCompression

--- disjoint_sets/disjoint_sets/disjoint_sets.cc
+++ disjoint_sets/disjoint_sets/disjoint_sets.cc
@@ -14,10 +14,10 @@ void DisjointSets::UnionBySize(int set1, int set2) {
   int root1 = Find(set1);
   int root2 = Find(set2);
   if (set[root2] < set[root1]) {
-    set[root2] += (set[root1] + 1);
+    set[root2] += set[root1];
     set[root1] = root2;
   } else {
-    set[root1] += (set[root2] + 1);
+    set[root1] += set[root2];
     set[root2] = root1;
   }
 }
