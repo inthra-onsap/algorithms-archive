dc41471322c46c0db52ba3c6b6aed50c59962ed6
inthra.onsap <inthra.onsap@gmail.com>
2017-06-18 07:18:20 +0700

    Implement LRU

--- memory_management/lru_cache/lru_cache.h
+++ memory_management/lru_cache/lru_cache.h
@@ -1,17 +1,60 @@
-
 #ifndef ALGORITHMS_ARCHIVE_LRU_CACHE_H_
 #define ALGORITHMS_ARCHIVE_LRU_CACHE_H_
 
+#include <list>
+#include <unordered_map>
+
 namespace algorithms_archive {
 namespace memory_management {
+
 template<typename KeyType, typename ValueType>
 class LruCache {
  public:
+  LruCache(int capacity_) : capacity{capacity_} {}
+
+  ValueType Get(KeyType key) {
+    auto item = cache.find(key);
+    if (item == cache.end()) {
+      return -1;
+    }
+    PromoteRecentlyUsed(item);
+    return item->second.first;
+  }
+
+  void Set(KeyType key, ValueType value) {
+    auto item = cache.find(key);
+    if (item != cache.end()) {
+      PromoteRecentlyUsed(item);
+      item->second.first = value;
+      return;
+    }
+
+    if (cache.size() >= capacity) {
+      cache.erase(lru_rank.back());
+      lru_rank.pop_back();
+    }
+
+    lru_rank.push_front(key);
+    cache.insert({key, {value, lru_rank.begin()}});
+  }
 
  private:
+  int capacity;
+  // <key, <value, key_iterator>>
+  std::unordered_map<KeyType, std::pair<ValueType, typename std::list<KeyType>::iterator>> cache;
+  std::list<KeyType> lru_rank;
 
+  void PromoteRecentlyUsed(typename std::unordered_map<
+      KeyType,
+      std::pair<ValueType,
+      typename std::list<KeyType>::iterator> >::iterator &item) {
+    lru_rank.erase(item->second.second);
+    lru_rank.push_front(item->first);
+    item->second.second = lru_rank.begin();
+  }
 };
-}
-}
+
+} // namespace memory_management
+} // namespace algorithms_archive
 
 #endif //ALGORITHMS_ARCHIVE_LRU_CACHE_H_
--- memory_management/lru_cache/lru_cache_test.cc
+++ memory_management/lru_cache/lru_cache_test.cc
@@ -7,8 +7,19 @@ namespace memory_management {
 class LruCacheTest : public testing::Test {
 };
 
-TEST_F(LruCacheTest, ExpectSortUnsortedArraySuccess) {
+TEST_F(LruCacheTest, ExpectInsertionSuccess) {
+  LruCache<int, int> lru(2);
+
+  lru.Set(1, 1);
+  EXPECT_EQ(1, lru.Get(1));
 }
 
+TEST_F(LruCacheTest, ExpectItemRemovedFromLruCacheSuccess) {
+  LruCache<int, int> lru(2);
+  lru.Set(1, 1);
+  lru.Set(2, 2);
+  lru.Set(3, 3);
+  EXPECT_EQ(-1, lru.Get(1));
+}
 }
 }
