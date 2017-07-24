d6f45a5a33d242c6fbacfe9ad8b8438c572ae8f0
inthra.onsap <inthra.onsap@gmail.com>
2017-07-25 04:41:55 +0700

    move code from header to .cc

--- string/knuth_morris_patt/knuth_morris_patt.cc
+++ string/knuth_morris_patt/knuth_morris_patt.cc
@@ -1,1 +1,30 @@
 #include "knuth_morris_patt.h"
+
+namespace algorithms_archive {
+namespace string {
+
+const std::vector<int> KnuthMorrisPatt::FindOccurences(std::string pattern) {
+  std::string str;
+  str = pattern + "$" + text;
+  std::vector<int> prefix_arr(str.length(), 0);
+  std::vector<int> occorrences;
+  ProcessPrefix(str, prefix_arr);
+  for (int i = (pattern.length() + 1); i < prefix_arr.size(); ++i)
+    if (pattern.length() == prefix_arr[i])
+      occorrences.push_back(i - (2 * pattern.length()));
+  return occorrences;
+}
+
+void KnuthMorrisPatt::ProcessPrefix(std::string &str, std::vector<int> &prefix_arr) {
+  prefix_arr[0] = 0;
+  int border = 0;
+  for (int i = 1; i < str.length(); ++i) {
+    while (border > 0 && str[i] != str[border])
+      border = prefix_arr[border - 1];
+    border = (str[i] == str[border]) ? border + 1 : 0;
+    prefix_arr[i] = border;
+  }
+}
+
+} // namespace string
+} // namespace algorithms_archive
--- string/knuth_morris_patt/knuth_morris_patt.h
+++ string/knuth_morris_patt/knuth_morris_patt.h
@@ -11,30 +11,12 @@ class KnuthMorrisPatt {
  public:
   KnuthMorrisPatt(std::string text_) : text{text_} {}
 
-  const std::vector<int> FindOccurences(std::string pattern) {
-    std::string str = pattern + "$" + text;
-    std::vector<int> prefix_arr(str.length(), 0);
-    std::vector<int> occorrences;
-    ProcessPrefix(str, prefix_arr);
-    for (int i = (pattern.length() + 1); i < prefix_arr.size(); ++i)
-      if (pattern.length() == prefix_arr[i])
-        occorrences.push_back(i - (2 * pattern.length()));
-    return occorrences;
-  }
+  const std::vector<int> FindOccurences(std::string pattern);
 
  private:
   std::string text;
 
-  void ProcessPrefix(std::string &str, std::vector<int> &prefix_arr) {
-    prefix_arr[0] = 0;
-    int border = 0;
-    for (int i = 1; i < str.length(); ++i) {
-      while (border > 0 && str[i] != str[border])
-        border = prefix_arr[border - 1];
-      border = (str[i] == str[border]) ? border + 1 : 0;
-      prefix_arr[i] = border;
-    }
-  }
+  void ProcessPrefix(std::string &str, std::vector<int> &prefix_arr);
 };
 
 } // namespace string
