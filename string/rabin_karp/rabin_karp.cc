e81dd63e4052dd49890023735d5944e0e7f1f020
inthra.onsap <inthra.onsap@gmail.com>
2017-09-21 14:50:16 +0700

    Split header and implement files

--- string/rabin_karp/rabin_karp.cc
+++ string/rabin_karp/rabin_karp.cc
@@ -3,5 +3,40 @@
 namespace algorithms_archive {
 namespace string {
 
+long long RabinKarp::Hash(std::string str) {
+  long long hash = 0;
+  for (int i = 0; i < str.length(); ++i) {
+    hash += str[i] * std::pow(kPrimeNumber, i);
+  }
+  return hash;
+}
+
+long long RabinKarp::Rehash(long long hash, std::string str, int start, int length) {
+  if((start - 1) < 0){
+    return hash;
+  }
+  return ((hash - str[start - 1]) / kPrimeNumber) +
+      (str[start + length - 1] * std::pow(kPrimeNumber, length - 1));
+}
+
+std::vector<int> RabinKarp::PatternMatching(std::string pattern, std::string text) {
+  std::vector<int> all_occurrences;
+  if (pattern.length() > text.length()) {
+    return all_occurrences;
+  }
+
+  long long hashed_pattern = Hash(pattern);
+  long long hashed_runner = Hash(text.substr(0, pattern.size()));
+  int l_limit = text.size() - pattern.size();
+  for (int i = 0; i <= l_limit; ++i) {
+    hashed_runner = Rehash(hashed_runner, text, i, pattern.length());
+    if (hashed_pattern == hashed_runner) {
+      all_occurrences.push_back(i);
+    }
+  }
+
+  return all_occurrences;
+}
+
 } // namespace string
 } // namespace algorithms_archive
--- string/rabin_karp/rabin_karp.h
+++ string/rabin_karp/rabin_karp.h
@@ -11,40 +11,11 @@ namespace string {
 
 class RabinKarp {
  public:
-  long long Hash(std::string str) {
-    long long hash = 0;
-    for (int i = 0; i < str.length(); ++i) {
-      hash += str[i] * std::pow(kPrimeNumber, i);
-    }
-    return hash;
-  }
+  long long Hash(std::string str);
 
-  long long Rehash(long long hash, std::string str, int start, int length) {
-    if((start - 1) < 0){
-      return hash;
-    }
-    return ((hash - str[start - 1]) / kPrimeNumber) +
-        (str[start + length - 1] * std::pow(kPrimeNumber, length - 1));
-  }
+  long long Rehash(long long hash, std::string str, int start, int length);
 
-  std::vector<int> PatternMatching(std::string pattern, std::string text) {
-    std::vector<int> all_occurrences;
-    if (pattern.length() > text.length()) {
-      return all_occurrences;
-    }
-
-    long long hashed_pattern = Hash(pattern);
-    long long hashed_runner = Hash(text.substr(0, pattern.size()));
-    int l_limit = text.size() - pattern.size();
-    for (int i = 0; i <= l_limit; ++i) {
-      hashed_runner = Rehash(hashed_runner, text, i, pattern.length());
-      if (hashed_pattern == hashed_runner) {
-        all_occurrences.push_back(i);
-      }
-    }
-
-    return all_occurrences;
-  }
+  std::vector<int> PatternMatching(std::string pattern, std::string text);
 
  private:
   const int kPrimeNumber = 13;
